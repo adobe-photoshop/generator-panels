bfaca9743aa0b895fcbd2e968d096f5c857257fb
jpeterso <jp@adobe.com>
2014-05-01 15:48:07 -0700

    Now reading the config file.

--- generatorconfig/js/generatorconfig.js
+++ generatorconfig/js/generatorconfig.js
@@ -9,34 +9,31 @@
 // Uncomment for debugger on load (Disabled in CEP 4.2)
 //window.__adobe_cep__.showDevTools();
 
+var config = require("./js/config")
+
+function loadConfig()
+{
+    var currentConfig = config.getConfig();
+    if (currentConfig && currentConfig["generator-assets"]) {
+        var optionList = Object.keys(currentConfig["generator-assets"]);
+        optionList.forEach( function( opt ) {
+            $("#" + opt).prop('checked', currentConfig["generator-assets"][opt] );
+        });
+    }
+}
+
 function initialize()
 {
     initColors();
+    loadConfig();
+    
 }
-
-
-function updateSample() {
-	var params = getParams();
-	var scaleTxt = (params.scale == "100%") ? "" : (params.scale + " ");
-	$("#samplename").text( scaleTxt + sampleLayerName + params.suffix );
-}
-
-// Menu / control handlers
-
-
 
 $(".configchk").change( function(checkEvent) {
     var itemID = this.id;
 	var value = ($("#" + itemID).is(":checked"));
     var msg = value ? " is checked" : " is not checked";
     alert( itemID + msg );
-});
-
-$("#saveconfig").click( function() {
-    // Call function defined in host/RenameLayers.jsx
-    // The path to this script is defined in the manifest.xml file.
-//    csInterface.evalScript("layerOps.doRename(" + JSON.stringify( getParams() ) + ");");
-    alert("Saving...");
 });
 
 // These are just developer shortcuts; they shouldn't appear in final code.
