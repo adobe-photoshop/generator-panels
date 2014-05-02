868cbb220a7af26409c2478a958f5c07fee111e8
jpeterso <jp@adobe.com>
2014-05-02 16:13:59 -0700

    Minor cleanup (makecert, readme)

--- cert/makecert.bat
+++ cert/makecert.bat
@@ -1,2 +1,14 @@
+rem sample script to produce certificate files
+
+rem set your email address here
+set email=%USERNAME%@example.com
+
+rem voodoo from http://stackoverflow.com/questions/7809648
+rem will probably fail if your computer isn't part of a Windows domain
+FOR /F "tokens=1 delims=" %%A in ('net user "%USERNAME%" /domain ^| find /I "Full Name"') do SET fullNameText=%%A
+set fullName=%fullNameText:Full Name=%
+for /f "tokens=* delims= " %%a in ("%fullName%") do set fullName=%%a
+
 set /P certpass="Enter certificate password: " %=%
-ZXPSignCmd -selfSignedCert US California "Adobe Systems" "John Peterson" %certpass% panelcert.p12 -email jp@adobe.com -validityDays 120
+del panelcert.p12
+ZXPSignCmd -selfSignedCert US California %USERDOMAIN% "%fullName%" %certpass% panelcert.p12 -email %email% -validityDays 120
