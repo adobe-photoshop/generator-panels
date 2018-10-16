d639f752d0ce12f52af720f00d41e0b7419d5606
John Peterson <jp@adobe.com>
2018-10-15 18:18:28 -0700

    Update versions for Photoshop 20.x and CEP version 9.

--- installPanels.py
+++ installPanels.py
@@ -230,7 +230,7 @@ argparser.add_argument('--zip', '-z', action='store_true', default=False,
                        help="Create ZIP archives for BuildForge signing")
 argparser.add_argument('--debug', '-d', nargs='?', const='status', default=None, choices=['status', 'on', 'off'],
                        help="Enable panel without signing")
-argparser.add_argument('--version', '-v', default='8',
+argparser.add_argument('--version', '-v', default='9',
                        help="CEP Version for setting PanelDebugMode")
 argparser.add_argument('--run', '-r', action='store_true', default=False,
                        help="Launch Photoshop after copy")
